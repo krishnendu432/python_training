2e6b2864e2e54a6522fd6e0d08ad682c1104594b
DESKTOP-CQIPQHJ\Santu <krishnenduroy432@gmail.com>
2024-07-14 08:00:06 +0530

    This is another commit on 14/7 morning.

--- 29_files_read_write.py
+++ 29_files_read_write.py
@@ -1,12 +1,16 @@
 # This is the code to check the different operations in file
-#with open("D:\Krishnendu\user_info.txt", 'w') as file :
-    #file.write("This is test file using python")
-
-#with open("user_info.txt", 'a') as file:
+#with open("user_info.txt", 'a') as file :
+    #file.write("Manish : 847345776 \n" "Guru : 948824858 \n")
+with open("user_info.txt", 'r') as file :
+    user_read=file.readlines() # It will print as a list
+    print("The content of the file is:")
+    for info in user_read : # created a for loop to access the file
+        print(info)
+#with open("test.txt", 'a') as file:
     # write operation performed
     #file.write("This is additional content.\n I love to code in python. \n")
     # Read the content from the created file
-with open("user_info.txt", 'r') as file:
+#with open("test.txt", 'r') as file:
     # performed the read operation
-    content=file.read()
-    print(f"The content of the file is: \n {content}")
+    #content=file.read()
+    #print(f"The content of the file 'test.txt' is: \n {content}")
