56bfefe4a0169f899f26e0e24788ca6271ce3227
DESKTOP-CQIPQHJ\Santu <krishnenduroy432@gmail.com>
2024-07-14 20:51:54 +0530

    This is another commit on 14/7 evening.

--- 33_random_dice_game.py
+++ 33_random_dice_game.py
@@ -4,7 +4,7 @@ print("Game start !!!!!!!!!!")
 while True :
     # random.randint() is a pre define function.
     print(f" Your value is : {random.randint(1,6)}") # Generate the random value between 1 to 6
-    user_choice=input((" Enter your choice: If you want to continue press 'y' or if you want to close press 'n' :"))
+    user_choice=input(("Enter your choice: If you want to continue press 'y' or if you want to close press 'n':"))
     if user_choice.lower() == 'y':
         continue
     elif user_choice.lower() == 'n':
