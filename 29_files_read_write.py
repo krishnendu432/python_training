ba64623bf75b39f21079f5478a99a325803d0c7d
DESKTOP-CQIPQHJ\Santu <krishnenduroy432@gmail.com>
2024-07-14 07:24:19 +0530

    This is my commit on 14/7/2024 morning session

---
+++ 29_files_read_write.py
@@ -0,0 +1,6 @@
+# This is the code to check the different operations in file
+#with open("D:\Krishnendu\user_info.txt", 'w') as file :
+    #file.write("This is test file using python")
+
+with open("user_info.txt", 'a') as file:
+    file.write("This is additional content.\n")
--- another_print.py
+++
@@ -1,7 +0,0 @@
-c=int(input("Enter your number"))
-a=5
-b=10
-s=a+b+c
-print(s)
-print(type(a))
-print(type(b))
