d90e79b7f4260c4e6bdd88ef0b23cb22f769437a
DESKTOP-CQIPQHJ\Santu <krishnenduroy432@gmail.com>
2024-07-14 07:39:03 +0530

    This is another commit on 14/7 morning.

--- 29_files_read_write.py
+++ 29_files_read_write.py
@@ -2,5 +2,11 @@
 #with open("D:\Krishnendu\user_info.txt", 'w') as file :
     #file.write("This is test file using python")
 
-with open("user_info.txt", 'a') as file:
-    file.write("This is additional content.\n")
+#with open("user_info.txt", 'a') as file:
+    # write operation performed
+    #file.write("This is additional content.\n I love to code in python. \n")
+    # Read the content from the created file
+with open("user_info.txt", 'r') as file:
+    # performed the read operation
+    content=file.read()
+    print(f"The content of the file is: \n {content}")
