313bcd9964439e1d36dc4038e2e8b56f69af0cd3
DESKTOP-CQIPQHJ\Santu <krishnenduroy432@gmail.com>
2024-07-20 11:16:10 +0530

    This is another commit on 20/7 morning .

--- 39_weather_app.py
+++ 39_weather_app.py
@@ -4,6 +4,6 @@ lat=input("Enter the latitude of the area: ")
 lon=input("Enter the longitude of the area: ")
 url=f'http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid={API_Key}'
 url1=f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_Key}'
-#response=requests.get(url)
+response=requests.get(url)
 response=requests.get(url1)
 print(response)
---
+++ 41_Interest_calculate_external_module.py
@@ -0,0 +1,4 @@
+import pyttsx3
+voice=pyttsx3.init()
+voice.say("I am Krishnendu.")
+voice.runAndWait()
