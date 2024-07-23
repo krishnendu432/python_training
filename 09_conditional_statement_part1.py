87868f59002554a06e4d6fb68b4f1090f0306032
DESKTOP-CQIPQHJ\Santu <krishnenduroy432@gmail.com>
2024-07-23 22:44:42 +0530

    This is another commit on 23/7 evening .

--- 09_conditional_statement_part1.py
+++ 09_conditional_statement_part1.py
@@ -2,15 +2,21 @@
 # Individual subject '33' is the pass mark but for total average '40%' is required as a pass mark.
 Lan=int(input("Enter the marks of the 1st Language Subject: "))
 Eng=int(input("Enter the marks of the 1st English Subject: "))
-#Math=int(input("Enter the marks of the 1st Mathematics: "))
-#Science=int(input("Enter the marks of the 1st Science Subject: "))
-#Arts=int(input("Enter the marks of the 1st Arts Subject: "))
-total_marks=Lan+Eng
-avg=total_marks/2
-if (Lan > 33) and (Eng > 33) :
-    print("Subject wise the student is passed. ")
-    print(f"The marks of the student are\n Language : {Lan}, English : {Eng}")
-elif avg > 40 :
-    print(f"All total the student become passed {avg}")
+Math=int(input("Enter the marks of the 1st Mathematics: "))
+Science=int(input("Enter the marks of the 1st Science Subject: "))
+Arts=int(input("Enter the marks of the 1st Arts Subject: "))
+total_marks=Lan+Eng+Math+Science+Arts
+avg=total_marks/5
+if (avg>= 40) and (Lan >= 33) and (Eng >= 33) and (Math >= 33) and (Science>=33) and (Arts >=33)  :
+    print(f" The student is passed. The percentage of the student is {avg} ")
+    print(f"The marks of the student are\n Language : {Lan}, English : {Eng}, Math :{Math}, Science :{Science}, Arts: {Arts}")
 else:
-    print(f"all total the  student become failed {avg}")
+    print(f" The  student become failed. The average of the student is : {avg}")
+
+# Write a program where we will take input as a post from user.
+# We will check that the post is talking about predefine statement or not.
+post = input("Enter you post : ")
+if ("India".lower() in post.lower()) :
+    print("The post is about India.")
+else:
+    print("The post is not about India.")
